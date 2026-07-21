9b5fa8aeba105e059b48972456f6572a7a327b93
rrooigg <devnadifa@gmail.com>
2026-07-21 20:40:48 +0300

    create 'expenses' table

---
+++ database/migrations/2026_07_21_173538_create_expenses_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name', 200);
+            $table->integer('amount');
+            $table->string('category', 200);
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('expenses');
+    }
+};
