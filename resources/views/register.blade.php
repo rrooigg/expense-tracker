3593e454b45f34be5ec72f4832fa8106ff047c06
rrooigg <devnadifa@gmail.com>
2026-07-22 11:41:54 +0300

    change title

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -3,7 +3,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-  <title>Register Page</title>
+  <title>eXpnse</title>
 </head>
 <body>
   @auth
